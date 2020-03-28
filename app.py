59f03ed36d857ee9b647b7dfe8b4699f6ff2f078
Senkottuvelan <34165508+Senkottuvelan@users.noreply.github.com>
2020-03-28 23:10:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ APP = Flask(__name__)
 CORS(APP)
 EMPTY_LIST = []
 
-BASE_URL = os.getenv('BASE_URL', 'https://thepiratebay.org/')
+BASE_URL = os.getenv('BASE_URL', 'https://boatmy.top/')
 JSONIFY_PRETTYPRINT_REGULAR = True
 
 # Translation table for sorting filters
