2625e98e7394f07d4599a82c275b6312c35facf9
Senkottuvelan <34165508+Senkottuvelan@users.noreply.github.com>
2020-03-31 22:45:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ def search_torrents(term=None, page=0):
     sort_arg = sort_filters[request.args.get('sort')] if sort in sort_filters else ''
 
     url = BASE_URL + 'search/' + str(term) + '/' + str(page) + '/' + str(sort_arg)
-    return jsonify(parse_page(url)), 200
+    return parse_page(url), 200
 
 
 def parse_page(url, sort=None):
@@ -137,33 +137,14 @@ def parse_page(url, sort=None):
     table_present = soup.find('table', {'id': 'searchResult'})
     if table_present is None:
         return EMPTY_LIST
-    titles = parse_titles(soup)
+   # titles = parse_titles(soup)
     links = parse_links(soup)
-    magnets = parse_magnet_links(soup)
-    times, sizes, uploaders = parse_description(soup)
-    seeders, leechers = parse_seed_leech(soup)
-    cat, subcat = parse_cat(soup)
-    torrents = []
-    for torrent in zip(titles, magnets, times, sizes, uploaders, seeders, leechers, cat, subcat, links):
-        torrents.append({
-            'title': torrent[0],
-            'magnet': torrent[1],
-            'time': convert_to_date(torrent[2]),
-            'size': convert_to_bytes(torrent[3]),
-            'uploader': torrent[4],
-            'seeds': int(torrent[5]),
-            'leeches': int(torrent[6]),
-            'category': torrent[7],
-            'subcat': torrent[8],
-            'id': torrent[9],
-        })
-
-    if sort:
-        sort_params = sort.split('_')
-        torrents = sorted(torrents, key=lambda k: k.get(sort_params[0]), reverse=sort_params[1].upper() == 'DESC')
-
-    return torrents
-
+   # magnets = parse_magnet_links(soup)
+   # times, sizes, uploaders = parse_description(soup)
+   # seeders, leechers = parse_seed_leech(soup)
+   # cat, subcat = parse_cat(soup)
+   
+    return links
 
 def parse_magnet_links(soup):
     '''
@@ -174,13 +155,7 @@ def parse_magnet_links(soup):
     return magnets
 
 
-def parse_titles(soup):
-    '''
-    Returns list of titles of torrents from soup
-    '''
-    titles = soup.find_all(class_='detLink')
-    titles[:] = [title.get_text() for title in titles]
-    return titles
+
 
 
 def parse_links(soup):
@@ -192,85 +167,3 @@ def parse_links(soup):
     return links
 
 
-def parse_description(soup):
-    '''
-    Returns list of time, size and uploader from soup
-    '''
-    description = soup.find_all('font', class_='detDesc')
-    description[:] = [desc.get_text().split(',') for desc in description]
-    times, sizes, uploaders = map(list, zip(*description))
-    times[:] = [time.replace(u'\xa0', u' ').replace('Uploaded ', '') for time in times]
-    sizes[:] = [size.replace(u'\xa0', u' ').replace(' Size ', '') for size in sizes]
-    uploaders[:] = [uploader.replace(' ULed by ', '') for uploader in uploaders]
-    return times, sizes, uploaders
-
-
-def parse_seed_leech(soup):
-    '''
-    Returns list of numbers of seeds and leeches from soup
-    '''
-    slinfo = soup.find_all('td', {'align': 'right'})
-    seeders = slinfo[::2]
-    leechers = slinfo[1::2]
-    seeders[:] = [seeder.get_text() for seeder in seeders]
-    leechers[:] = [leecher.get_text() for leecher in leechers]
-    return seeders, leechers
-
-
-def parse_cat(soup):
-    '''
-    Returns list of category and subcategory
-    '''
-    cat_subcat = soup.find_all('center')
-    cat_subcat[:] = [c.get_text().replace('(', '').replace(')', '').split() for c in cat_subcat]
-    cat = [cs[0] for cs in cat_subcat]
-    subcat = [' '.join(cs[1:]) for cs in cat_subcat]
-    return cat, subcat
-
-
-def convert_to_bytes(size_str):
-    '''
-    Converts torrent sizes to a common count in bytes.
-    '''
-    size_data = size_str.split()
-
-    multipliers = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB']
-
-    size_magnitude = float(size_data[0])
-    multiplier_exp = multipliers.index(size_data[1])
-    size_multiplier = 1024 ** multiplier_exp if multiplier_exp > 0 else 1
-
-    return size_magnitude * size_multiplier
-
-
-def convert_to_date(date_str):
-    '''
-    Converts the dates into a proper standardized datetime.
-    '''
-    date_format = None
-
-    if re.search('^[0-9]+ min(s)? ago$', date_str.strip()):
-        minutes_delta = int(date_str.split()[0])
-        torrent_dt = datetime.now() - timedelta(minutes=minutes_delta)
-        date_str = '{}-{}-{} {}:{}'.format(torrent_dt.year, torrent_dt.month, torrent_dt.day, torrent_dt.hour, torrent_dt.minute)
-        date_format = '%Y-%m-%d %H:%M'
-
-    elif re.search('^[0-9]*-[0-9]*\s[0-9]+:[0-9]+$', date_str.strip()):
-        today = datetime.today()
-        date_str = '{}-'.format(today.year) + date_str
-        date_format = '%Y-%m-%d %H:%M'
-    
-    elif re.search('^Today\s[0-9]+\:[0-9]+$', date_str):
-        today = datetime.today()
-        date_str = date_str.replace('Today', '{}-{}-{}'.format(today.year, today.month, today.day))
-        date_format = '%Y-%m-%d %H:%M'
-    
-    elif re.search('^Y-day\s[0-9]+\:[0-9]+$', date_str):
-        today = datetime.today() - timedelta(days=1)
-        date_str = date_str.replace('Y-day', '{}-{}-{}'.format(today.year, today.month, today.day))
-        date_format = '%Y-%m-%d %H:%M'
-
-    else:
-        date_format = '%m-%d %Y'
-
-    return datetime.strptime(date_str, date_format)
